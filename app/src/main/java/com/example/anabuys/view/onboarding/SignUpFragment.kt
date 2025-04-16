e0bc7ae579e1bce3942dd84f8e28dc401cf052a1
Kalil <kalilvg9@gmail.com>
2025-04-16 00:05:59 -0600

    Se arregló el funcionamiento del registro

--- app/src/main/java/com/example/anabuys/view/onboarding/SignUpFragment.kt
+++ app/src/main/java/com/example/anabuys/view/onboarding/SignUpFragment.kt
@@ -1,12 +1,12 @@
 package com.example.anabuys.view.onboarding
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.anabuys.R
@@ -14,47 +14,26 @@ import com.example.anabuys.databinding.SignUpFragmentBinding
 import com.example.anabuys.utils.FragmentCommunicator
 import com.example.anabuys.viewModel.SignUpViewModel
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
 class SignUpFragment : Fragment() {
 
     private var _binding: SignUpFragmentBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
     private val viewModel by viewModels<SignUpViewModel>()
     private lateinit var communicator: FragmentCommunicator
-    var isValid: Boolean = false
+    private var isValid: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = SignUpFragmentBinding.inflate(inflater, container, false)
         communicator = requireActivity() as OnboardingActivity
         setupView()
         setupObservers()
         return binding.root
-
     }
 
-
-    private fun setupView(){
-
-        binding.registerButton.setOnClickListener{
-
-            viewModel.requestSignUp(binding.emailTIET.text.toString(),
-                binding.passwordTIET.text.toString())
-
-        }
-
-        binding.registerButton.setOnClickListener {
-
-            findNavController().navigate(R.id.action_SecondFragment_to_personalInformationFragment)
-        }
+    private fun setupView() {
 
         binding.registerButton.setOnClickListener {
             if (isValid) {
@@ -63,30 +42,45 @@ class SignUpFragment : Fragment() {
                 Toast.makeText(activity, "Ingreso invalido", Toast.LENGTH_SHORT).show()
             }
         }
+
         binding.emailTIET.addTextChangedListener {
-            if (binding.emailTIET.text.toString().isEmpty()) {
-                binding.emailTIET.error = "Por favor introduce un correo"
+            binding.emailTIET.error = if (it.isNullOrEmpty()) {
                 isValid = false
+                "Por favor introduce un correo"
             } else {
                 isValid = true
+                null
             }
         }
+
         binding.passwordTIET.addTextChangedListener {
-            if (binding.passwordTIET.text.toString().isEmpty()) {
-                binding.passwordTIET.error = "Por favor introduce una contraseña"
+            binding.passwordTIET.error = if (it.isNullOrEmpty()) {
                 isValid = false
+                "Por favor introduce una contraseña"
             } else {
                 isValid = true
+                null
             }
         }
-
-
     }
 
-    private fun setupObservers(){
+    private fun setupObservers() {
         viewModel.loaderState.observe(viewLifecycleOwner) { loaderState ->
             communicator.showLoader(loaderState)
         }
+
+        viewModel.signUpSuccess.observe(viewLifecycleOwner) { success ->
+            if (success) {
+                findNavController().navigate(R.id.action_SecondFragment_to_personalInformationFragment)
+            }
+        }
+    }
+
+    private fun requestRegister() {
+        viewModel.requestSignUp(
+            binding.emailTIET.text.toString(),
+            binding.passwordTIET.text.toString()
+        )
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -95,11 +89,6 @@ class SignUpFragment : Fragment() {
         binding.registerBack.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
         }
-    }
-
-    private fun requestRegister() {
-        viewModel.requestSignUp(binding.emailTIET.text.toString(),
-            binding.passwordTIET.text.toString())
     }
 
     override fun onDestroyView() {
